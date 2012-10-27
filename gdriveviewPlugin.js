b10c923ecabf9343dbd4507dce71781b8398fccf
Robert <rkuczaty@gmail.com>
2012-10-27 05:42:13 -0400

    js przykładowy

---
+++ gdriveviewPlugin.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+//plugin metadata
+var headers = {
+name: "Orion Markdown Example 1",
+version: "1.0",
+description: "Markdown plugin that provides link to markdown info site."
+};
+
+//create the plugin
+var provider = new orion.PluginProvider(headers);
+
+provider.registerServiceProvider("orion.page.link", {}, {
+name: "Markdown Intro",
+id: "orion.example1.pageLink.markDown",
+uriTemplate: "http://daringfireball.net/projects/markdown/"
+});
+
+provider.connect();
+};
+
+
